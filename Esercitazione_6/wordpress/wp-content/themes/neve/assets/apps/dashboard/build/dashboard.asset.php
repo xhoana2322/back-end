182e8aa9631e0baebc85cbeb3095b7a2314b8412
xhoana2322 <xhoana.beqiri23@gmail.com>
2024-02-14 15:41:34 +0100

    aggiornamento

---
+++ Esercitazione_6/wordpress/wp-content/themes/neve/assets/apps/dashboard/build/dashboard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('neve-components', 'react', 'wp-api', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fc689c0d85f58ca664bce9b3eab70965');
