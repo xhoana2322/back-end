f7bdc999d98f5072147ebf922b60f9467c0a9af0
xhoana2322 <xhoana.beqiri23@gmail.com>
2024-02-19 14:45:13 +0100

    agg

--- Esercitazione_10_API/assets/js/script.js
+++ Esercitazione_10_API/assets/js/script.js
@@ -1,4 +1,4 @@
-const urlAPI = "http://localhost/Progetto-settimana-2_back-end/wordpress/wp-json/wp/v2/posts";
+const urlAPI = "http://localhost/Progetto-settimana-2_back-end/wordpress/wp-json/wp/v2/";
 
 fetch(urlAPI)
     .then(resp => resp.json())
